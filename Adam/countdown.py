71fcd39069193a6ad92018f327f385410a188a02
Adam-Gillich <gillich.adam04@gmail.com>
2023-10-24 20:44:29 +0200

    YOu can input the value, made an input_integer form of input_float

--- Adam/countdown.py
+++ Adam/countdown.py
@@ -3,8 +3,21 @@
 # os.system('cls||clear')
 
 import time
+from termcolor import cprint
 
-time_ = int(input('What time do you want to count down from? '))
+
+def input_integer(text):
+    while True:
+        try:
+            value = int(input(text).strip())
+        except ValueError:
+            cprint('\nYou can only use whole numbers!\n', color='red', force_color=True)
+            continue
+        return value
+
+
+time_ = input_integer('What time do you want to count down from?\nPlease Enter seconds: ')
+print()
 
 for i in range(time_, 0, -1):
     seconds = i % 60
@@ -12,3 +25,5 @@ for i in range(time_, 0, -1):
     hours = int(i / 3600) % 24
     print(f'\r{hours}:{minutes: 03}:{seconds: 03}', end='')
     time.sleep(1)
+print('\rProcess finished!')
+input('\nPres Enter to finish! ')
