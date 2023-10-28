733c8d30a348213d570772dcb01b029bb83ab7ed
Adam-Gillich <gillich.adam04@gmail.com>
2023-10-28 20:39:08 +0200

    A Python challange

---
+++ Adam/challange_1.py
@@ -0,0 +1,46 @@
+'''
+Write a function that takes a sentence, and returns the longest even-length word in the sentence.
+Or if it has no even-length words, return "00"
+'''
+
+
+txt = 'to check if a list is empty in Python?'
+text2 = "lmdd."
+
+
+def to_remove(x, addlist):
+    if x == "?":
+        addlist.remove(x)
+    if x == "!":
+        addlist.remove(x)
+    if x == ".":
+        addlist.remove(x)
+
+
+def counter(text):
+    word = []
+    longest_word = []
+    for i in text:
+        word.append(i)
+        to_remove(i, word)
+        if i == " ":
+            word.remove(i)
+        if i == " " or i == "?" or i == "!" or i == "." or i == ",":
+            if len(longest_word) < len(word) and len(word) % 2 == 0:
+                longest_word.clear()
+                for i in word:
+                    longest_word.append(i)
+            word.clear()
+    if len(longest_word) == 0:
+        return "00"
+    else:
+        return longest_word
+
+
+print(''.join(counter(input('Give a text: '))))
+
+"""
+repls = ('hello', 'goodbye'), ('world', 'earth')
+s = 'hello, world'
+reduce(lambda a, kv: a.replace(*kv), repls, s)
+"""
